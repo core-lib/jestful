114d0ad94795d9d7e582f66414988ddc505b34eb
core-lib <yangchangpei@yangchaeidemini.lan>
2016-05-05 12:33:47 +0800

    Add AcceptHeaderDecider

---
+++ jestful-client/src/main/java/org/qfox/jestful/client/AcceptHeaderDecider.java
@@ -0,0 +1,54 @@
+package org.qfox.jestful.client;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.qfox.jestful.commons.MediaType;
+import org.qfox.jestful.core.Accepts;
+import org.qfox.jestful.core.Action;
+import org.qfox.jestful.core.Actor;
+import org.qfox.jestful.core.BeanContainer;
+import org.qfox.jestful.core.Initialable;
+import org.qfox.jestful.core.Request;
+import org.qfox.jestful.core.ResponseDeserializer;
+
+/**
+ * <p>
+ * Description:
+ * </p>
+ * 
+ * <p>
+ * Company: 广州市俏狐信息科技有限公司
+ * </p>
+ * 
+ * @author Payne 646742615@qq.com
+ *
+ * @date 2016年5月5日 下午12:29:59
+ *
+ * @since 1.0.0
+ */
+public class AcceptHeaderDecider implements Actor, Initialable {
+	private final Map<MediaType, ResponseDeserializer> map = new HashMap<MediaType, ResponseDeserializer>();
+
+	public Object react(Action action) throws Exception {
+		Accepts produces = action.getProduces();
+		Accepts supports = new Accepts(map.keySet());
+		if (produces.isEmpty() == false) {
+			supports.retainAll(produces);
+		}
+		Request request = action.getRequest();
+		request.setRequestHeader("Accept", supports.toString());
+		return action.execute();
+	}
+
+	public void initialize(BeanContainer beanContainer) {
+		Collection<ResponseDeserializer> deserializers = beanContainer.find(ResponseDeserializer.class).values();
+		for (ResponseDeserializer deserializer : deserializers) {
+			String contentType = deserializer.getContentType();
+			MediaType mediaType = MediaType.valueOf(contentType);
+			map.put(mediaType, deserializer);
+		}
+	}
+
+}
