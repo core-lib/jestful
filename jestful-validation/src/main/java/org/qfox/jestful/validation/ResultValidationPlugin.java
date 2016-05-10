09cc3871af9f81cf67e51660b888004aa0636c0f
core-lib <yangchangpei@yangchaeidemini.lan>
2016-05-10 12:31:26 +0800

    Add validation module

---
+++ jestful-validation/src/main/java/org/qfox/jestful/validation/ResultValidationPlugin.java
@@ -0,0 +1,70 @@
+package org.qfox.jestful.validation;
+
+import java.lang.reflect.Method;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.ValidationException;
+import javax.validation.ValidatorFactory;
+import javax.validation.executable.ExecutableValidator;
+
+import org.qfox.jestful.core.Action;
+import org.qfox.jestful.core.Actor;
+
+/**
+ * <p>
+ * Description:
+ * </p>
+ * 
+ * <p>
+ * Company: 广州市俏狐信息科技有限公司
+ * </p>
+ * 
+ * @author Payne 646742615@qq.com
+ *
+ * @date 2016年5月10日 下午12:22:18
+ *
+ * @since 1.0.0
+ */
+public class ResultValidationPlugin implements Actor {
+	private ValidatorFactory validatorFactory;
+
+	public ResultValidationPlugin() {
+		super();
+		this.validatorFactory = Validation.buildDefaultValidatorFactory();
+	}
+
+	public ResultValidationPlugin(ValidatorFactory validatorFactory) {
+		super();
+		this.validatorFactory = validatorFactory;
+	}
+
+	public Object react(Action action) throws Exception {
+		Object value = action.execute();
+		Object target = action.getResource().getController();
+		Method method = action.getMapping().getMethod();
+		Object result = action.getResult().getValue();
+		ExecutableValidator validator = validatorFactory.getValidator().forExecutables();
+		Set<ConstraintViolation<Object>> violations = validator.validateReturnValue(target, method, result);
+
+		if (violations != null && !violations.isEmpty()) {
+			StringBuilder message = new StringBuilder();
+			for (ConstraintViolation<Object> violation : violations) {
+				message.append(violation.getPropertyPath()).append(" : ").append(violation.getMessage()).append(" ; ");
+			}
+			throw new ValidationException(message.toString());
+		}
+
+		return value;
+	}
+
+	public ValidatorFactory getValidatorFactory() {
+		return validatorFactory;
+	}
+
+	public void setValidatorFactory(ValidatorFactory validatorFactory) {
+		this.validatorFactory = validatorFactory;
+	}
+
+}
