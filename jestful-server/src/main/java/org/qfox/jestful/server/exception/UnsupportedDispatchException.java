2487651144b808c79ee26db0d259e358cd7cd765
core-lib <646742615@qq.com>
2016-11-20 22:23:22 +0800

    Not important

--- jestful-server/src/main/java/org/qfox/jestful/server/exception/UnsupportedDispatchException.java
+++ jestful-server/src/main/java/org/qfox/jestful/server/exception/UnsupportedDispatchException.java
@@ -1,20 +1,21 @@
 package org.qfox.jestful.server.exception;
 
-import org.qfox.jestful.core.exception.JestfulException;
-
 import javax.servlet.DispatcherType;
+
+import org.qfox.jestful.core.exception.JestfulException;
 
 /**
  * Created by yangchangpei on 16/10/8.
  */
 public class UnsupportedDispatchException extends JestfulException {
-    private final DispatcherType dispatcherType;
+	private static final long serialVersionUID = -1318182255899791582L;
+	private final DispatcherType dispatcherType;
 
-    public UnsupportedDispatchException(DispatcherType dispatcherType) {
-        this.dispatcherType = dispatcherType;
-    }
+	public UnsupportedDispatchException(DispatcherType dispatcherType) {
+		this.dispatcherType = dispatcherType;
+	}
 
-    public DispatcherType getDispatcherType() {
-        return dispatcherType;
-    }
+	public DispatcherType getDispatcherType() {
+		return dispatcherType;
+	}
 }
