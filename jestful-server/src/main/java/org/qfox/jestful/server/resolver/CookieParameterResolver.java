d54e71e2639450066b782df2a43aa721f34dce20
yangchangpei <646742615@qq.com>
2016-11-20 10:54:27 +0800

    Bug Fixes

--- jestful-server/src/main/java/org/qfox/jestful/server/resolver/CookieParameterResolver.java
+++ jestful-server/src/main/java/org/qfox/jestful/server/resolver/CookieParameterResolver.java
@@ -1,78 +1,70 @@
 package org.qfox.jestful.server.resolver;
 
-import java.net.URLDecoder;
-import java.util.List;
+import org.qfox.jestful.core.*;
+import org.qfox.jestful.core.converter.StringConversion;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-
-import org.qfox.jestful.core.Action;
-import org.qfox.jestful.core.Actor;
-import org.qfox.jestful.core.BeanContainer;
-import org.qfox.jestful.core.Initialable;
-import org.qfox.jestful.core.Parameter;
-import org.qfox.jestful.core.Position;
-import org.qfox.jestful.core.Request;
-import org.qfox.jestful.core.converter.StringConversion;
+import java.net.URLDecoder;
+import java.util.List;
 
 /**
  * <p>
  * Description:
  * </p>
- * 
+ * <p>
  * <p>
  * Company: 广州市俏狐信息科技有限公司
  * </p>
- * 
+ *
  * @author Payne 646742615@qq.com
- *
  * @date 2016年4月20日 上午11:03:06
- *
  * @since 1.0.0
  */
 public class CookieParameterResolver implements Actor, Initialable {
-	private StringConversion cookieStringConversion;
-	private boolean caseInsensitive = true;
+    private StringConversion cookieStringConversion;
+    private boolean caseInsensitive = true;
 
-	public Object react(Action action) throws Exception {
-		Request request = action.getRequest();
-		if (request instanceof HttpServletRequest == false) {
-			return action.execute();
-		}
-		String charset = action.getHeaderEncodeCharset();
-		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-		List<Parameter> parameters = action.getParameters().all(Position.COOKIE);
-		for (Parameter parameter : parameters) {
-			Cookie[] cookies = httpServletRequest.getCookies();
-			for (Cookie cookie : cookies) {
-				if (caseInsensitive ? cookie.getName().equalsIgnoreCase(parameter.getName()) == false : cookie.getName().equals(parameter.getName()) == false) {
-					continue;
-				}
-				String source = URLDecoder.decode(cookie.getValue(), charset);
-				cookieStringConversion.convert(parameter, source);
-			}
-		}
-		return action.execute();
-	}
+    public Object react(Action action) throws Exception {
+        Request request = action.getRequest();
+        if (request instanceof HttpServletRequest == false) {
+            return action.execute();
+        }
+        String charset = action.getHeaderEncodeCharset();
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        List<Parameter> parameters = action.getParameters().all(Position.COOKIE);
+        for (Parameter parameter : parameters) {
+            Cookie[] cookies = httpServletRequest.getCookies();
+            for (int i = 0; cookies != null && i < cookies.length; i++) {
+                Cookie cookie = cookies[i];
+                if (caseInsensitive ? cookie.getName().equalsIgnoreCase(parameter.getName()) == false : cookie.getName().equals(parameter.getName()) == false) {
+                    continue;
+                }
+                String source = URLDecoder.decode(cookie.getValue(), charset);
+                cookieStringConversion.convert(parameter, source);
+            }
+        }
+        return action.execute();
+    }
 
-	public void initialize(BeanContainer beanContainer) {
-		this.cookieStringConversion = beanContainer.get(StringConversion.class);
-	}
+    public void initialize(BeanContainer beanContainer) {
+        this.cookieStringConversion = beanContainer.get(StringConversion.class);
+    }
 
-	public boolean isCaseInsensitive() {
-		return caseInsensitive;
-	}
+    public boolean isCaseInsensitive() {
+        return caseInsensitive;
+    }
 
-	public void setCaseInsensitive(boolean caseInsensitive) {
-		this.caseInsensitive = caseInsensitive;
-	}
+    public void setCaseInsensitive(boolean caseInsensitive) {
+        this.caseInsensitive = caseInsensitive;
+    }
 
-	public StringConversion getCookieStringConversion() {
-		return cookieStringConversion;
-	}
+    public StringConversion getCookieStringConversion() {
+        return cookieStringConversion;
+    }
 
-	public void setCookieStringConversion(StringConversion cookieStringConversion) {
-		this.cookieStringConversion = cookieStringConversion;
-	}
+    public void setCookieStringConversion(StringConversion cookieStringConversion) {
+        this.cookieStringConversion = cookieStringConversion;
+    }
 
 }
