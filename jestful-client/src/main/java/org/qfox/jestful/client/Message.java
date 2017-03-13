2316168232c426aecd7a620214eeaf76086655ca
yangchangpei <646742615@qq.com>
2017-03-13 18:50:54 +0800

    Add Message.java And MessageScheduler.java

---
+++ jestful-client/src/main/java/org/qfox/jestful/client/Message.java
@@ -0,0 +1,70 @@
+package org.qfox.jestful.client;
+
+import org.qfox.jestful.commons.collection.CaseInsensitiveMap;
+import org.qfox.jestful.core.Response;
+import org.qfox.jestful.core.Status;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * Created by yangchangpei on 17/3/13.
+ */
+public final class Message<T> implements Serializable {
+    private static final long serialVersionUID = -2348643625659731238L;
+
+    private final boolean success;
+    private final Status status;
+    private final Map<String, String> header = new CaseInsensitiveMap<String, String>();
+    private final T entity;
+    private final Throwable throwable;
+
+    public Message(Response response, T entity) throws IOException {
+        this.success = true;
+        this.status = response != null ? response.getResponseStatus() : null;
+        for (String key : response != null && response.getHeaderKeys() != null ? response.getHeaderKeys() : new String[0]) {
+            String name = key != null ? key : "";
+            String value = response != null ? response.getResponseHeader(key) : null;
+            this.header.put(name, value);
+        }
+        this.entity = entity;
+        this.throwable = null;
+    }
+
+    public Message(Response response, Throwable throwable) throws IOException {
+        this.success = false;
+        this.status = response != null ? response.getResponseStatus() : null;
+        for (String key : response != null && response.getHeaderKeys() != null ? response.getHeaderKeys() : new String[0]) {
+            String name = key != null ? key : "";
+            String value = response != null ? response.getResponseHeader(key) : null;
+            this.header.put(name, value);
+        }
+        this.entity = null;
+        this.throwable = throwable;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+
+    public Map<String, String> getHeader() {
+        return header;
+    }
+
+
+    public T getEntity() {
+        return entity;
+    }
+
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+
+}
---
+++ jestful-client/src/main/java/org/qfox/jestful/client/scheduler/MessageScheduler.java
@@ -0,0 +1,49 @@
+package org.qfox.jestful.client.scheduler;
+
+import org.qfox.jestful.client.Client;
+import org.qfox.jestful.client.exception.UncertainBodyTypeException;
+import org.qfox.jestful.core.Action;
+import org.qfox.jestful.client.Message;
+import org.qfox.jestful.core.Response;
+import org.qfox.jestful.core.Result;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * Created by yangchangpei on 17/3/13.
+ */
+public class MessageScheduler implements Scheduler {
+
+    @Override
+    public boolean supports(Action action) {
+        Result result = action.getResult();
+        Class<?> klass = result.getKlass();
+        return klass == Message.class;
+    }
+
+    @Override
+    public Type getBodyType(Client client, Action action) throws UncertainBodyTypeException {
+        Result result = action.getResult();
+        Type type = result.getType();
+        if (type instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+            Type actualTypeArgument = parameterizedType.getActualTypeArguments()[0];
+            return actualTypeArgument;
+        } else {
+            throw new UncertainBodyTypeException(client, action);
+        }
+    }
+
+    @Override
+    public Object schedule(Client client, Action action) throws Exception {
+        try {
+            Object entity = action.execute();
+            Response response = action.getResponse();
+            return new Message<Object>(response, entity);
+        } catch (Throwable throwable) {
+            Response response = action.getResponse();
+            return new Message<Object>(response, throwable);
+        }
+    }
+}
