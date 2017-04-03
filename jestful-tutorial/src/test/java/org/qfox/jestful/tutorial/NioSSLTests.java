d0b3a06d1c118df68d79e0f761088967929d17d7
core-lib <wan20100101>
2017-04-04 01:59:02 +0800

    Nio SSL Supports Completed

--- jestful-tutorial/src/test/java/org/qfox/jestful/tutorial/NioSSLTests.java
+++ jestful-tutorial/src/test/java/org/qfox/jestful/tutorial/NioSSLTests.java
@@ -6,6 +6,7 @@ import org.qfox.jestful.client.scheduler.Callback;
 import org.qfox.jestful.commons.Lock;
 import org.qfox.jestful.commons.SimpleLock;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -13,21 +14,28 @@ import java.net.URL;
  * Version: 1.0
  */
 public class NioSSLTests {
+    ProxyAPI proxyAPI = NioClient.builder().setConnTimeout(1000 * 1000).setEndpoint(new URL("https://merchant.qfoxy.com/v4/login")).build().create(ProxyAPI.class);
+
+    public NioSSLTests() throws MalformedURLException {
+    }
 
     @Test
     public void test() throws Exception {
         Lock lock = new SimpleLock();
-        ProxyAPI proxyAPI = NioClient.builder().setConnTimeout(1000 * 1000).setEndpoint(new URL("https://merchant.qfoxy.com/v4/login")).build().create(ProxyAPI.class);
+
         proxyAPI.index(new Callback<String>() {
             @Override
             public void onCompleted(boolean success, String result, Throwable throwable) {
-                System.out.print(result);
-                lock.openAll();
+                try {
+                    test();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
             public void onSuccess(String result) {
-
+                System.out.println(result);
             }
 
             @Override
