87a9768705f0362ae984bf49aa34cd3a2bef6d4f
yangchangpei <646742615@qq.com>
2017-07-07 16:06:54 +0800

    Add Extendable Value Conversion

--- jestful-server/src/main/java/org/qfox/jestful/server/converter/BeanConverter.java
+++ jestful-server/src/main/java/org/qfox/jestful/server/converter/BeanConverter.java
@@ -9,10 +9,7 @@ import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BeanConverter implements Converter {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -31,21 +28,50 @@ public class BeanConverter implements Converter {
         } catch (Exception e) {
             throw new UnsupportedConversionException(e, name, clazz, map, provider);
         }
+        Set<String> converteds = new HashSet<String>();
         for (String key : map.keySet()) {
+            if (converteds.contains(key)) continue;
+
             if (key.startsWith(name + ".") && key.length() > name.length() + 1) {
                 int index = key.indexOf('.');
                 String property = key.substring(index + 1);
-                String[] values = map.get(key) != null ? map.get(key).clone() : new String[0];
-                Map<String, String[]> _map = new LinkedHashMap<String, String[]>();
-                _map.put(property, values);
-                try {
-                    PropertyDescriptor descriptor = new PropertyDescriptor(property.split("\\.")[0], clazz);
-                    Object value = provider.convert(property.split("\\.")[0], descriptor.getPropertyType(), decoded, charset, _map);
-                    descriptor.getWriteMethod().invoke(bean, value);
-                } catch (ConversionException e) {
-                    throw e;
-                } catch (Exception e) {
-                    logger.warn("fail to convert property [" + property + "] of class [" + clazz + "] with values " + Arrays.toString(values), e);
+                // 如果还是个复合类型
+                if (property.contains(".")) {
+                    int i = property.indexOf('.');
+                    String p = property.substring(0, i);
+
+                    Map<String, String[]> _map = new LinkedHashMap<String, String[]>();
+                    for (String k : map.keySet()) {
+                        if (k.startsWith(name + "." + p + ".") && k.length() > (name + "." + p).length() + 1) {
+                            String[] values = map.get(k) != null ? map.get(k).clone() : new String[0];
+                            _map.put(k.substring((name + ".").length()), values);
+                            converteds.add(k);
+                        }
+                    }
+                    try {
+                        PropertyDescriptor descriptor = new PropertyDescriptor(p, clazz);
+                        Object value = provider.convert(p, descriptor.getPropertyType(), decoded, charset, _map);
+                        descriptor.getWriteMethod().invoke(bean, value);
+                    } catch (ConversionException e) {
+                        throw e;
+                    } catch (Exception e) {
+                        logger.warn("fail to convert property [" + property + "] of class [" + clazz + "] with values " + _map, e);
+                    }
+                }
+                // 如果只是个简单类型
+                else {
+                    String[] values = map.get(key) != null ? map.get(key).clone() : new String[0];
+                    Map<String, String[]> _map = new LinkedHashMap<String, String[]>();
+                    _map.put(property, values);
+                    try {
+                        PropertyDescriptor descriptor = new PropertyDescriptor(property, clazz);
+                        Object value = provider.convert(property, descriptor.getPropertyType(), decoded, charset, _map);
+                        descriptor.getWriteMethod().invoke(bean, value);
+                    } catch (ConversionException e) {
+                        throw e;
+                    } catch (Exception e) {
+                        logger.warn("fail to convert property [" + property + "] of class [" + clazz + "] with values " + Arrays.toString(values), e);
+                    }
                 }
             }
         }
@@ -68,26 +94,54 @@ public class BeanConverter implements Converter {
             throw new UnsupportedConversionException(e, name, type, map, provider);
         }
         List<?> variables = Arrays.asList(clazz.getTypeParameters());
+        Set<String> converteds = new HashSet<String>();
         for (String key : map.keySet()) {
+            if (converteds.contains(key)) continue;
+
             if (key.startsWith(name + ".") && key.length() > name.length() + 1) {
                 int index = key.indexOf('.');
                 String property = key.substring(index + 1);
-                String[] values = map.get(key) != null ? map.get(key).clone() : new String[0];
-                Map<String, String[]> _map = new LinkedHashMap<String, String[]>();
-                _map.put(property, values);
-                try {
-                    PropertyDescriptor descriptor = new PropertyDescriptor(property.split("\\.")[0], clazz);
-                    Type _type = descriptor.getReadMethod().getGenericReturnType();
-                    if (_type instanceof TypeVariable<?>) {
-                        int _index = variables.indexOf(_type);
-                        _type = type.getActualTypeArguments()[_index];
+                // 如果还是个复合类型
+                if (property.contains(".")) {
+                    int i = property.indexOf('.');
+                    String p = property.substring(0, i);
+
+                    Map<String, String[]> _map = new LinkedHashMap<String, String[]>();
+                    for (String k : map.keySet()) {
+                        if (k.startsWith(name + "." + p + ".") && k.length() > (name + "." + p).length() + 1) {
+                            String[] values = map.get(k) != null ? map.get(k).clone() : new String[0];
+                            _map.put(k.substring((name + ".").length()), values);
+                            converteds.add(k);
+                        }
                     }
-                    Object value = provider.convert(property.split("\\.")[0], _type, decoded, charset, _map);
-                    descriptor.getWriteMethod().invoke(bean, value);
-                } catch (ConversionException e) {
-                    throw e;
-                } catch (Exception e) {
-                    throw new UnknownConversionException(e, name, clazz, map, provider);
+                    try {
+                        PropertyDescriptor descriptor = new PropertyDescriptor(p, clazz);
+                        Type _type = descriptor.getReadMethod().getGenericReturnType();
+                        if (_type instanceof TypeVariable<?>) _type = type.getActualTypeArguments()[variables.indexOf(_type)];
+                        Object value = provider.convert(p, _type, decoded, charset, _map);
+                        descriptor.getWriteMethod().invoke(bean, value);
+                    } catch (ConversionException e) {
+                        throw e;
+                    } catch (Exception e) {
+                        logger.warn("fail to convert property [" + property + "] of class [" + clazz + "] with values " + _map, e);
+                    }
+                }
+                // 如果只是个简单类型
+                else {
+                    String[] values = map.get(key) != null ? map.get(key).clone() : new String[0];
+                    Map<String, String[]> _map = new LinkedHashMap<String, String[]>();
+                    _map.put(property, values);
+                    try {
+                        PropertyDescriptor descriptor = new PropertyDescriptor(property, clazz);
+                        Type _type = descriptor.getReadMethod().getGenericReturnType();
+                        if (_type instanceof TypeVariable<?>) _type = type.getActualTypeArguments()[variables.indexOf(_type)];
+                        Object value = provider.convert(property, _type, decoded, charset, _map);
+                        descriptor.getWriteMethod().invoke(bean, value);
+                    } catch (ConversionException e) {
+                        throw e;
+                    } catch (Exception e) {
+                        logger.warn("fail to convert property [" + property + "] of class [" + clazz + "] with values " + Arrays.toString(values), e);
+                    }
                 }
             }
         }
