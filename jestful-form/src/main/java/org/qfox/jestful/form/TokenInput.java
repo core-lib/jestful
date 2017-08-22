347a6ef3b4b5fbeb0dc72ca3869041ad6006dd03
yangchangpei <646742615@qq.com>
2017-08-22 17:16:45 +0800

    Add Anti Form Repeat Submit Supports

--- jestful-form/src/main/java/org/qfox/jestful/form/TokenInput.java
+++ jestful-form/src/main/java/org/qfox/jestful/form/TokenInput.java
@@ -3,14 +3,18 @@ package org.qfox.jestful.form;
 import org.qfox.jestful.core.BeanContainer;
 
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.DynamicAttributes;
 import javax.servlet.jsp.tagext.TagSupport;
-import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by yangchangpei on 17/8/21.
  */
-public class TokenInput extends TagSupport {
+public class TokenInput extends TagSupport implements DynamicAttributes {
     private static final long serialVersionUID = 8901103747024632711L;
+
+    private final Map<String, Object> attributes = new LinkedHashMap<String, Object>();
 
     @Override
     public int doStartTag() throws JspException {
@@ -19,16 +23,16 @@ public class TokenInput extends TagSupport {
             TokenManager tokenManager = beanContainer.get(TokenManager.class);
             String token = tokenManager.create();
 
-            if (getValue("type") == null) setValue("type", "hidden");
-            if (getValue("name") == null) setValue("name", "form-token");
-            if (getValue("value") == null) setValue("value", token);
+            if (!attributes.containsKey("name")) attributes.put("name", "form-token");
+            if (!attributes.containsKey("type")) attributes.put("type", "hidden");
+            if (!attributes.containsKey("value")) attributes.put("value", token);
+
             StringBuilder sb = new StringBuilder();
             sb.append("<input");
 
-            Enumeration<String> names = getValues();
-            while (names.hasMoreElements()) {
-                String name = names.nextElement();
-                Object value = getValue(name);
+            for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+                String name = entry.getKey();
+                Object value = entry.getValue();
                 if (value == null) continue;
                 else sb.append(" ");
                 sb.append(name).append("=").append("\"").append(value.toString()).append("\"");
@@ -42,4 +46,14 @@ public class TokenInput extends TagSupport {
         }
     }
 
+    @Override
+    public void setDynamicAttribute(String uri, String localName, Object value) throws JspException {
+        attributes.put(localName, value);
+    }
+
+    @Override
+    public void release() {
+        attributes.clear();
+        super.release();
+    }
 }
