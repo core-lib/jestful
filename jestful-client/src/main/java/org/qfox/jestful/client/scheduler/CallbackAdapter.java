fbebf6ce29e652869e0ca87f9931765eb451efee
yangchangpei <646742615@qq.com>
2017-10-19 15:02:47 +0800

    Client Request Retry Supports

--- jestful-client/src/main/java/org/qfox/jestful/client/scheduler/CallbackAdapter.java
+++ jestful-client/src/main/java/org/qfox/jestful/client/scheduler/CallbackAdapter.java
@@ -3,15 +3,15 @@ package org.qfox.jestful.client.scheduler;
 /**
  * Created by yangchangpei on 17/10/18.
  */
-public abstract class CallbackAdapter<T> implements Callback<T> {
+public abstract class CallbackAdapter<R> implements Callback<R> {
 
     @Override
-    public void onCompleted(boolean success, T result, Exception exception) {
+    public void onCompleted(boolean success, R result, Exception exception) {
 
     }
 
     @Override
-    public void onSuccess(T result) {
+    public void onSuccess(R result) {
 
     }
 
