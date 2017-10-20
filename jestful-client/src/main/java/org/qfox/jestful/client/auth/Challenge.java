9d66ad1e2679d1ef3c20a84577158b4afbd431e9
core-lib <646742615@qq.com>
2017-10-21 01:07:23 +0800

    Client Promise Pattern Supports

--- jestful-client/src/main/java/org/qfox/jestful/client/auth/Challenge.java
+++ jestful-client/src/main/java/org/qfox/jestful/client/auth/Challenge.java
@@ -1,18 +1,43 @@
 package org.qfox.jestful.client.auth;
 
+import java.io.Serializable;
 import java.util.Map;
 
 /**
  * Created by Payne on 2017/10/20.
  */
-public interface Challenge {
+public class Challenge implements Serializable {
+    private static final long serialVersionUID = 1139030198376990893L;
 
-    Provoker getProvoker();
+    private final Provoker provoker;
+    private final String algorithm;
+    private final String realm;
+    private final Map<String, String> properties;
 
-    String getAlgorithm();
+    public Challenge(Provoker provoker, String algorithm, String realm, Map<String, String> properties) {
+        this.provoker = provoker;
+        this.algorithm = algorithm;
+        this.realm = realm;
+        this.properties = properties;
+    }
 
-    String getRealm();
+    public static Challenge valueOf(String authenticate) {
+        return null;
+    }
 
-    Map<String, String> getParameters();
+    public Provoker getProvoker() {
+        return provoker;
+    }
 
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public String getRealm() {
+        return realm;
+    }
+
+    public Map<String, String> getProperties() {
+        return properties;
+    }
 }
