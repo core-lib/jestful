82863b3d034bbad7f6f0b9bc9c7228733fff4482
core-lib <646742615@qq.com>
2017-11-26 11:31:00 +0800

    Make LoopedNioBalancer Become Concurrent

--- jestful-client-nio/src/main/java/org/qfox/jestful/client/nio/balancer/LoopedNioBalancer.java
+++ jestful-client-nio/src/main/java/org/qfox/jestful/client/nio/balancer/LoopedNioBalancer.java
@@ -4,25 +4,30 @@ import org.qfox.jestful.client.nio.NioClient;
 import org.qfox.jestful.client.nio.NioProcessor;
 import org.qfox.jestful.core.Action;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by yangchangpei on 17/4/11.
  */
 public class LoopedNioBalancer implements NioBalancer {
-    private final Map<NioProcessor[], Integer> map = new ConcurrentHashMap<NioProcessor[], Integer>();
+    private final ConcurrentMap<NioProcessor[], AtomicInteger> map = new ConcurrentHashMap<NioProcessor[], AtomicInteger>();
 
     @Override
     public void dispatch(Action action, NioClient client, NioProcessor[] processors) {
-        if (processors == null || processors.length == 0) throw new IllegalArgumentException("processors is empty");
-        synchronized (processors) {
-            Integer index = map.get(processors);
-            if (index == null) map.put(processors, index = 0);
-            else if (index == processors.length) index = 0;
-            processors[index].process(action);
-            map.put(processors, ++index);
+        if (processors == null || processors.length == 0) throw new IllegalArgumentException("processors is null or empty");
+        AtomicInteger index = map.get(processors);
+        if (index == null) {
+            AtomicInteger old = map.putIfAbsent(processors, index = new AtomicInteger(-1));
+            if (old != null) index = old;
         }
+        int i = index.incrementAndGet();
+        while (i >= processors.length) {
+            index.compareAndSet(i, 0);
+            i = index.get();
+        }
+        processors[i].process(action);
     }
 
 }
--- jestful-sample/src/test/java/org/qfox/jestful/sample/user/RedirectSampleAPITest.java
+++ jestful-sample/src/test/java/org/qfox/jestful/sample/user/RedirectSampleAPITest.java
@@ -2,10 +2,13 @@ package org.qfox.jestful.sample.user;
 
 import org.junit.Test;
 import org.qfox.jestful.client.Client;
+import org.qfox.jestful.client.nio.NioClient;
 import org.qfox.jestful.client.redirect.Redirector;
 import org.qfox.jestful.commons.Lock;
 import org.qfox.jestful.commons.SimpleLock;
 import org.qfox.jestful.sample.RedirectSampleAPI;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by yangchangpei on 17/9/27.
@@ -14,24 +17,27 @@ public class RedirectSampleAPITest {
 
     @Test
     public void get() throws Exception {
-        RedirectSampleAPI userAPI = Client.builder()
+        RedirectSampleAPI userAPI = NioClient.builder()
                 .setProtocol("http")
                 .setHostname("localhost")
                 .setPort(8080)
-                .setKeepAlive(null)
+                .setKeepAlive(true)
                 .build()
                 .creator()
                 .addBackPlugins(new Redirector())
                 .create(RedirectSampleAPI.class);
 
-        User user = userAPI.source();
-        System.out.println(user);
-
         Lock lock = new SimpleLock();
-        userAPI.source((success, result, exception) -> {
-            System.out.println(result);
-            lock.openAll();
-        });
+        AtomicInteger remain = new AtomicInteger(100);
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                User user = userAPI.source();
+//                System.out.println(user);
+                if (remain.decrementAndGet() == 0) {
+                    lock.openAll();
+                }
+            }).start();
+        }
         lock.lockOne();
     }
 
