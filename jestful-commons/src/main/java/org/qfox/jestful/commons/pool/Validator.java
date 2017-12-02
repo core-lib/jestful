9806bb60b9eabbba662efabdc89432ec3d897d16
core-lib <646742615@qq.com>
2017-12-02 11:46:55 +0800

    Connection Close Clock Supports

--- jestful-commons/src/main/java/org/qfox/jestful/commons/pool/Validator.java
+++ jestful-commons/src/main/java/org/qfox/jestful/commons/pool/Validator.java
@@ -4,6 +4,12 @@ public interface Validator<Item> {
 
     boolean validate(Item item);
 
+    /**
+     * calculate the item's timeout delay in milliseconds
+     *
+     * @param item the item
+     * @return the timeout delay in milliseconds of the item
+     */
     long timeout(Item item);
 
 }
