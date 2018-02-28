85fa35b6f9018e1490b777c7d1bd0d09a714e711
YangChangPei <646742615@qq.com>
2018-02-28 15:59:10 +0800

    1. whether context-path append while redirect is configurable

--- jestful-server/src/main/java/org/qfox/jestful/server/renderer/RedirectRenderer.java
+++ jestful-server/src/main/java/org/qfox/jestful/server/renderer/RedirectRenderer.java
@@ -1,16 +1,25 @@
 package org.qfox.jestful.server.renderer;
 
 import org.qfox.jestful.core.*;
+import org.qfox.jestful.core.exception.BeanConfigException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * Created by Payne on 2017/4/27.
  */
-public class RedirectRenderer implements Renderer, Initialable {
+public class RedirectRenderer implements Renderer, Initialable, Configurable {
     private String context = "";
     private String command = "@redirect:";
+    private boolean ignored = false;
+
+    @Override
+    public void config(Map<String, String> arguments) throws BeanConfigException {
+        String ignored = arguments.get("context-path-ignored");
+        this.ignored = "true".equalsIgnoreCase(ignored) || "yes".equalsIgnoreCase(ignored) || "1".equals(ignored);
+    }
 
     public void initialize(BeanContainer beanContainer) {
         ServletContext servletContext = beanContainer.get(ServletContext.class);
@@ -26,8 +35,9 @@ public class RedirectRenderer implements Renderer, Initialable {
     public void render(Action action, Object value, Request request, Response response) throws Exception {
         String text = (String) value;
         String path = text.substring(command.length());
-        path = path.contains("://") ? path : context + path;
+        path = path.contains("://") ? path : ignored ? path : context + path;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         httpServletResponse.sendRedirect(path);
     }
+
 }
