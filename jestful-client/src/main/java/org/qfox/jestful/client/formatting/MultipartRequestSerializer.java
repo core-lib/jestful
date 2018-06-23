82632940d3c06ecbcf17c36df6f4ad00dc91e0a0
core-lib <646742615@qq.com>
2018-06-23 11:46:10 +0800

    统一转换和反转换

--- jestful-client/src/main/java/org/qfox/jestful/client/formatting/MultipartRequestSerializer.java
+++ jestful-client/src/main/java/org/qfox/jestful/client/formatting/MultipartRequestSerializer.java
@@ -52,7 +52,6 @@ public class MultipartRequestSerializer implements RequestSerializer, Initialabl
     public boolean supports(Action action) {
         List<Parameter> bodies = action.getParameters().all(Position.BODY);
         return bodies.size() == 0 || bodies.size() != 1 || supports(bodies.get(0));
-        return bodies.size() == 0 || bodies.size() > 1 || supports(bodies.get(0));
     }
 
     public boolean supports(Parameter parameter) {
