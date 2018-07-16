9d0f761b60730f36766b9c40f739c0d99840cf5e
YangChangPei <646742615@qq.com>
2018-07-16 12:27:42 +0800

    1. 客户端处理 @Matrix 注解的参数

--- jestful-sample/src/main/java/org/qfox/jestful/sample/IndexAPI.java
+++ jestful-sample/src/main/java/org/qfox/jestful/sample/IndexAPI.java
@@ -12,7 +12,7 @@ public interface IndexAPI {
     String matrix(
             @Path("路径") String path,
             @Path("矩阵") String matrix,
-            @Matrix("姓名") String[] names,
+            @Matrix(value = "姓名", path = "路径") String[] names,
             @Query("查询") String query,
             @Header("请求头") String header,
             @Cookie("饼干") String cookie
